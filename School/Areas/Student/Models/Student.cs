8cd50f6f8a4e38d8a9bc5531ed9026395bc7a4e9
Nhat Trung <incursio40@gmail.com>
2023-03-18 18:39:44 +0700

    update student

---
+++ School/Areas/Student/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Areas.Student.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime Dob { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal Salary { get; set; }
+        public int No { get; set; }
+        // đứng từ employee lấy danh sách từ departments (optional)
+        public virtual Departments? Departments { get; set; }
+    }
+}
