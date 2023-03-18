3bcea444c25e3773c8fa3d8a7644bf489a2663df
KhaiCuong <duongkhaicuong@gmail.com>
2023-03-18 18:09:32 +0700

    sdasdas

---
+++ School/Areas/Class/Controllers/ClassController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace School.Areas.Class.Controllers
+{
+    public class ClassController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Duong khai Cuong
+            return View();
+        }
+    }
+}
